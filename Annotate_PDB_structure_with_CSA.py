d088368308c14b8b9d0c35aee73f14533377bf3f
Thomas Rich <snowboarder2958@gmail.com>
2014-02-26 18:40:43 -0800

    auto commit

--- Annotate_PDB_structure_with_CSA.py
+++ Annotate_PDB_structure_with_CSA.py
@@ -266,6 +266,7 @@ for alignment in blast_record.alignments:
             pdbHits = hit_checker(csaResDict,hsp.query,hsp.query_start,numberedResidues,numberedResiduesDict,hsp.sbjct,hsp.sbjct_start)
             
             if pdbHits != {}:
+                print '----------------------------------------------------------'
                 print 'Residue Hits:'
                 print pdbHits
                 for item in hsp.__dict__.items():
